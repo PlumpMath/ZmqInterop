c584dc4cd31dc4e51e44b920ff91547a3eebec19
AlexForster <alex@alexforster.com>
2013-04-12 21:33:30 -0400

    Project ZmqInterop created

---
+++ ZmqInterop/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZmqInterop")]
+[assembly: AssemblyProduct("ZmqInterop")]
+[assembly: AssemblyCopyright("Copyright © 2013 Alex Forster")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.1.13102")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
